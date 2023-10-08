66b818ce56da5c153dcce806536a34c53f09c9eb
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2023-10-08 17:56:36 +0900

    微修正

--- lib/notify-aws-billing-stack.ts
+++ lib/notify-aws-billing-stack.ts
@@ -4,8 +4,7 @@ import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaPython from '@aws-cdk/aws-lambda-python-alpha';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as lambdaPython from "@aws-cdk/aws-lambda-python-alpha";
 
 export class NotifyAwsBillingStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,42 +12,50 @@ export class NotifyAwsBillingStack extends Stack {
 
     // Parameters
     const { accountId, region } = new ScopedAws(this);
-    const sysName = this.node.tryGetContext('sysName') as string;
-    const notifyDaysInterval = this.node.tryGetContext('notifyDaysInterval') as string;
-    const slackWebhookUrlPath = this.node.tryGetContext('slackWebhookUrlPath') as string | "";
-    const lineAccessTokenPath = this.node.tryGetContext('lineAccessTokenPath') as string | "";
+    const sysName = this.node.tryGetContext("sysName") as string;
+    const notifyDaysInterval = this.node.tryGetContext(
+      "notifyDaysInterval"
+    ) as string;
+    const slackWebhookUrlPath = this.node.tryGetContext(
+      "slackWebhookUrlPath"
+    ) as string | "";
+    const lineAccessTokenPath = this.node.tryGetContext(
+      "lineAccessTokenPath"
+    ) as string | "";
 
     let appAccessInfo = {};
-    let secretArn: string;
+    let secretArn: string = "";
 
     if (slackWebhookUrlPath) {
-      secretArn = `arn:aws:secretsmanager:${region}:${accountId}:secret:${slackWebhookUrlPath}-*`
+      secretArn = `arn:aws:secretsmanager:${region}:${accountId}:secret:${slackWebhookUrlPath}-*`;
       appAccessInfo = {
-        SLACK_WEBHOOK_URL_PATH: slackWebhookUrlPath
+        SLACK_WEBHOOK_URL_PATH: slackWebhookUrlPath,
       };
     }
 
     if (lineAccessTokenPath) {
-      secretArn = `arn:aws:secretsmanager:${region}:${accountId}:secret:${lineAccessTokenPath}-*`
+      secretArn = `arn:aws:secretsmanager:${region}:${accountId}:secret:${lineAccessTokenPath}-*`;
       appAccessInfo = {
-        LINE_ACCESS_TOKEN_PATH: lineAccessTokenPath
+        LINE_ACCESS_TOKEN_PATH: lineAccessTokenPath,
       };
     }
 
     // IAM Role
-    const notifyAwsBillingRole = new iam.Role(this, 'notifyAwsBillingRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    const notifyAwsBillingRole = new iam.Role(this, "notifyAwsBillingRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
       roleName: `${sysName}-notify-aws-billing-role`,
       managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AWSLambdaBasicExecutionRole"
+        ),
       ],
       inlinePolicies: {
-        'NotifySlackToBilling': new iam.PolicyDocument({
+        NotifyAwsBilling: new iam.PolicyDocument({
           statements: [
             new iam.PolicyStatement({
               effect: iam.Effect.ALLOW,
-              actions: ['ce:GetCostAndUsage'],
-              resources: ['*'],
+              actions: ["ce:GetCostAndUsage"],
+              resources: ["*"],
             }),
             new iam.PolicyStatement({
               effect: iam.Effect.ALLOW,
@@ -60,36 +67,39 @@ export class NotifyAwsBillingStack extends Stack {
       },
     });
 
-    // Lambda function environment
-
-
     // Lambda layer
-    const paramsAndSecrets =
-      lambda.ParamsAndSecretsLayerVersion.fromVersion(
-        lambda.ParamsAndSecretsVersions.V1_0_103,
-        {
-          cacheSize: 500,
-          logLevel: lambda.ParamsAndSecretsLogLevel.DEBUG,
-        }
-      );
-
+    const paramsAndSecrets = lambda.ParamsAndSecretsLayerVersion.fromVersion(
+      lambda.ParamsAndSecretsVersions.V1_0_103,
+      {
+        cacheSize: 500,
+        logLevel: lambda.ParamsAndSecretsLogLevel.DEBUG,
+      }
+    );
 
     // Lambda function
-    const notifyAwsBillingFunction = new lambdaPython.PythonFunction(this, 'function', {
-      functionName: `${sysName}-notify-aws-billing-function`,
-      entry: 'lib/function',
-      runtime: lambda.Runtime.PYTHON_3_10,
-      role: notifyAwsBillingRole,
-      timeout: Duration.seconds(10),
-      logRetention: 365,
-      environment: appAccessInfo,
-      paramsAndSecrets: paramsAndSecrets,
-    });
+    const notifyAwsBillingFunction = new lambdaPython.PythonFunction(
+      this,
+      "function",
+      {
+        functionName: `${sysName}-notify-aws-billing-function`,
+        entry: "lib/function",
+        runtime: lambda.Runtime.PYTHON_3_10,
+        role: notifyAwsBillingRole,
+        timeout: Duration.seconds(10),
+        logRetention: 365,
+        environment: appAccessInfo,
+        paramsAndSecrets: paramsAndSecrets,
+      }
+    );
 
     // EventBridge rule
-    new events.Rule(this, 'notifyAwsBillingRule', {
+    new events.Rule(this, "notifyAwsBillingRule", {
       ruleName: `${sysName}-notify-aws-billing-rule`,
-      schedule: events.Schedule.cron({ day: `*/${notifyDaysInterval}`, hour: '0', minute: '0' }),
+      schedule: events.Schedule.cron({
+        day: `*/${notifyDaysInterval}`,
+        hour: "0",
+        minute: "0",
+      }),
       targets: [new targets.LambdaFunction(notifyAwsBillingFunction)],
     });
   }
