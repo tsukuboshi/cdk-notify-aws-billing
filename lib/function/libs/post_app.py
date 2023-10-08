e3f0ae62210960e853c6ba65314ac5aa4a244773
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2023-10-08 18:08:37 +0900

    LINE通知関数を修正

--- lib/function/libs/post_app.py
+++ lib/function/libs/post_app.py
@@ -1,12 +1,12 @@
 import json
 import os
-import urllib.request
+from urllib import request, parse
 
 
 def post_slack(title: str, detail: str) -> None:
     # https://api.slack.com/messaging/webhooks#getting_started
     url = get_secret(os.environ.get('SLACK_WEBHOOK_URL_PATH'), 'info')
-    data = {
+    payload = {
         "text": title,
         "blocks": [
             {
@@ -35,6 +35,7 @@ def post_slack(title: str, detail: str) -> None:
             }
         ]
     }
+    data = json.dumps(payload).encode()
 
     try:
         send_post_request(url, data)
@@ -47,7 +48,10 @@ def post_line(title: str, detail: str) -> None:
     line_access_token = get_secret(os.environ.get('LINE_ACCESS_TOKEN_PATH'), 'info')
 
     url = "https://notify-api.line.me/api/notify"
-    data = {'message': f'{title}\n\n{detail}'}
+    payload = {
+        "message": f"{title}\n\n{detail}"
+        }
+    data = parse.urlencode(payload).encode("utf-8")
     headers = {"Authorization": "Bearer %s" % line_access_token}
 
     try:
@@ -57,17 +61,16 @@ def post_line(title: str, detail: str) -> None:
 
 
 def send_post_request(url: str, data, headers={}) -> None:
-    data_encoded = json.dumps(data).encode()
-    req = urllib.request.Request(url, data=data_encoded, headers=headers, method='POST')
-    with urllib.request.urlopen(req) as response:
+    req = request.Request(url, data=data, headers=headers, method='POST')
+    with request.urlopen(req) as response:
         print(response.status)
 
 
 def get_secret(secret_name, secret_key):
     secrets_extension_endpoint = "http://localhost:2773/secretsmanager/get?secretId=" + secret_name
     headers = {"X-Aws-Parameters-Secrets-Token": os.environ.get('AWS_SESSION_TOKEN')}
-    secrets_extension_req = urllib.request.Request(secrets_extension_endpoint, headers=headers)
-    with urllib.request.urlopen(secrets_extension_req) as response:
+    secrets_extension_req = request.Request(secrets_extension_endpoint, headers=headers)
+    with request.urlopen(secrets_extension_req) as response:
         secret_config = response.read()
     secret_json = json.loads(secret_config)['SecretString']
     secret_value = json.loads(secret_json)[secret_key]
